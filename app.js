98ea9f28ae9c576c64803e85a14b6c66c6513c43
Полина <chesnova.07@outlook.com>
2025-04-22 01:48:36 +0300

    теперь иожно рисовать но пока только одним цветом и размером

---
+++ app.js
@@ -0,0 +1,40 @@
+const canvas = document.getElementById("jscanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.lineWight = 2.5;
+ctx.StrokeStyle = "rgb(5, 5, 5)";
+
+canvas.height = 600; // обязательно задать размер и тут, иначе мышь-линия не совпадают
+canvas.width = 800;
+
+let painting = false;  // рисуем или нет - по умолчанию нет
+
+function startPainting() {
+    painting = true;
+}
+
+function onMouseMove(event) { // функция которая отвечает за отслеживание курсора
+    x = event.offsetX;
+    y = event.offsetY; //offset - позиция курсора внутри canvasa (самого окна рисования) 
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown(event) { // отслеживание нажатие курсора
+    painting = true;
+}
+function onMouseUp(event) { // отслеживание "отпускания" курсора
+    painting = false;
+}
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", onMouseUp);
+}
